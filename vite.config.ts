20f7cbca2c4060acb8393f14625b3471b1188a7e
David França <davidemanuel.cf@gmail.com>
2022-06-06 16:30:06 -0300

    Domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { cors: { origin: "http://64.227.27.136:3001/" } },
+  server: { cors: { origin: ["https://www.issuesystemdf.software/", "*"] } },
 });
