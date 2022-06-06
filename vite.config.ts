e5adc9303ca8acba530d8b0e2ce877d85a7b3e21
David França <davidemanuel.cf@gmail.com>
2022-06-06 14:44:30 -0300

    Cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: { cors: { origin: "http://64.227.27.136:3002/" } },
+});
