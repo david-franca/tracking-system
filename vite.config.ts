f70b81cb9ea9c149ff8fa10cb35a6eaa4e66a107
David França <davidemanuel.cf@gmail.com>
2022-06-06 14:44:53 -0300

    Port backend

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { cors: { origin: "http://64.227.27.136:3002/" } },
+  server: { cors: { origin: "http://64.227.27.136:3001/" } },
 });
